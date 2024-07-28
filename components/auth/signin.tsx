1e15bbf44117e561b7f3d03ce0a3378c09d86d7a
choiSeokYoon <ctd0606@naver.com>
2024-07-28 11:09:46 +0800

    Feat : 로그인기능 구현

--- components/auth/signin.tsx
+++ components/auth/signin.tsx
@@ -1,15 +1,37 @@
 "use client";
 
 import { Button, Input } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
+import { createBrowserSupabaseClient } from "utils/supabase/client";
 
 export default function Signin({ setView }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const supabase = createBrowserSupabaseClient();
+
+  const signInMutation = useMutation({
+    mutationFn: async () => {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      if (data) {
+        console.log(data);
+      }
+      if (error) {
+        //TODO 알림창 라이브러리 활용
+        alert(error.message);
+      }
+    },
+  });
   return (
     <div className="flex flex-col w-96">
       <div className="pt-8 pb-6 px-10 w-full flex flex-col items-center justify-center max-w-3xl bg-white gap-2 rounded-lg">
-        <h2 className="mb-6 text-2xl font-bold text-center text-gray-800">로그인</h2>
+        <h2 className="mb-6 text-2xl font-bold text-center text-gray-800">
+          로그인
+        </h2>
         <Input
           value={email}
           onChange={(e) => setEmail(e.target.value)}
@@ -22,7 +44,14 @@ export default function Signin({ setView }) {
           label="password"
           type="password"
         />
-        <Button className="mt-2">
+        <Button
+          className="mt-2"
+          onClick={() => {
+            signInMutation.mutate();
+          }}
+          disabled={signInMutation.isPending}
+          loading={signInMutation.isPending}
+        >
           로그인
         </Button>
       </div>
