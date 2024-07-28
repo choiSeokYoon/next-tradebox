a3154ce91b64fdc6575129f1951bba8fcc9c8dc1
choiSeokYoon <ctd0606@naver.com>
2024-07-28 08:38:14 +0800

    Feat : supabase 연동

---
+++ utils/supabase/client.ts
@@ -0,0 +1,9 @@
+"use client";
+//supabase SDK
+import { createBrowserClient } from "@supabase/ssr";
+
+export const createBrowserSupabaseClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
