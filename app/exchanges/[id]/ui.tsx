69b31a45eb3a405266b8e8395095afca5a4dadab
choiSeokYoon <ctd0606@naver.com>
2024-07-30 09:14:59 +0800

    Fix : 이미지 없을 시 렌더링 구성하지 않음 - 다른 이미지 대체

--- app/exchanges/[id]/ui.tsx
+++ app/exchanges/[id]/ui.tsx
@@ -3,17 +3,27 @@
 import Image from "next/image";
 
 export default function UI({ exchangeProduct }) {
-    console.log(exchangeProduct.exchange_images[0].image_url)
+  const imageUrl = exchangeProduct.exchange_images?.[0]?.image_url;
   return (
     <div className="w-full pt-4 pl-64 pr-64 flex flex-col pb-10">
       <div className="flex items-center justify-center">
-        <Image
-          src={exchangeProduct.exchange_images[0].image_url}
-          alt="교환 물건 이미지"
-          width={500}
-          height={500}
-          className="rounded-lg"
-        />
+        {imageUrl ? (
+          <Image
+            src={exchangeProduct.exchange_images[0].image_url}
+            alt="교환 물건 이미지"
+            width={500}
+            height={500}
+            className="rounded-lg"
+          />
+        ) : (
+          <Image
+            src={"/images/image-not-found.png"}
+            width={500}
+            height={500}
+            alt={"교환 물건 이미지"}
+            className="rounded-lg"
+          />
+        )}
       </div>
 
       <ul className="space-y-2 pt-10">
@@ -21,8 +31,8 @@ export default function UI({ exchangeProduct }) {
           {exchangeProduct.title}
         </li>
         <li className="text-gray-600">
-            <p>{exchangeProduct.created_at}</p>
-            <p>{exchangeProduct.location}</p>
+          <p>{exchangeProduct.created_at}</p>
+          <p>{exchangeProduct.location}</p>
         </li>
         <li className="text-gray-600">{exchangeProduct.category}</li>
         <li className="text-gray-600">{exchangeProduct.item_condition}</li>
