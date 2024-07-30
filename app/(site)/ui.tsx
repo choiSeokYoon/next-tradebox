cc7ae91aaae2a3caf175718d52e0605ae43fba7a
choiSeokYoon <ctd0606@naver.com>
2024-07-30 10:35:52 +0800

    Feat : 유저 정보 활용 - 유저정보 로컬스토리지에 저장

--- app/(site)/ui.tsx
+++ app/(site)/ui.tsx
@@ -1,10 +1,19 @@
 "use client";
 
+import { useEffect } from "react";
 import Category from "./components/category";
 import ExchangeList from "./components/exchange-list";
+import { useRecoilState } from "recoil";
+import { userEmailState } from "recoil/atoms";
 
 
-export default function UI() {
+export default function UI({session}) {
+  const [userEmail, setUserEmail] = useRecoilState(userEmailState)
+  useEffect(()=>{
+    setUserEmail(session?.user?.email)
+  },[session])
+
+ 
   return (
     <div className="w-full">
       <Category/>
--- recoil/atoms.ts
+++ recoil/atoms.ts
@@ -1,6 +1,18 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist({
+  key: "user",
+  storage: localStorage,
+});
+
+export const userEmailState = atom({
+  key: "userEmailState",
+  default: "",
+  effects_UNSTABLE: [persistAtom],
+});
 
 export const searchState = atom({
-    key : "searchState",
-    default: ""
-})
+  key: "searchState",
+  default: "",
+});
