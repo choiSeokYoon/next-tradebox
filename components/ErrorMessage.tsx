1ec2900237ea042c33c0eab7ca76424c90db9b1c
choiSeokYoon <ctd0606@naver.com>
2024-08-03 17:48:02 +0800

    Feat : 로딩, 에러 컴포넌트 생성

---
+++ components/ErrorMessage.tsx
@@ -0,0 +1,8 @@
+
+export default function ErrorMessage() {
+  return (
+    <p className="text-center text-red-600 py-4">
+      목록을 불러오는데 실패했습니다.
+    </p>
+  );
+}
---
+++ components/Loading.tsx
@@ -0,0 +1,6 @@
+
+export default function Loading() {
+  return (
+    <p className="text-center text-gray-600 py-4">로딩 중...</p>
+  )
+}
