8f64837942e117af4548af8f13fe895a5c61bae2
choiSeokYoon <ctd0606@naver.com>
2024-08-12 01:44:03 +0800

    Fix : 배포환경에서 채팅방 생성 시 조건 에러 안먹음 - toast 대신 alert 창으로 대체

--- hooks/query/useChat.ts
+++ hooks/query/useChat.ts
@@ -39,8 +39,7 @@ export const useCreateChatRoom = () => {
         alert("이미 채팅한 기록이 있습니다. 채팅 목록으로 이동합니다.");
         router.push("/chat-list");
       } else {
-        console.error("채팅 방 생성 중 오류 발생:", error);
-        toast.error("채팅 방 생성 중 오류가 발생했습니다. 다시 시도해 주세요.");
+        alert("채팅방 생성에 실패했습니다.");
       }
     },
   });
